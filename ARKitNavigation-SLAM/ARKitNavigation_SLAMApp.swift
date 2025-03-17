c2625100f81db2a03607437cc0b56477856c029d
khoanisdev <doanvankhoan124@gmail.com>
2025-03-17 14:12:41 +0700

    Initial Commit

---
+++ ARKitNavigation-SLAM/ARKitNavigation_SLAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitNavigation_SLAMApp.swift
+//  ARKitNavigation-SLAM
+//
+//  Created by Đoàn Văn Khoan on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ARKitNavigation_SLAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
